f6b8f05537b401431ccd9d1c64c4988058448fa0
Gustav Jakobsson <Gustav.Jakobsson@abbindustrigymnasium.se>
2020-10-06 15:13:55 +0200

    hdhk

--- main.js
+++ main.js
@@ -7,15 +7,37 @@ function run () {
 
 let database = firebase.database();
 
+
+
 database.ref("live/testRum").on("value", snap => {
   temperature = snap.val()["temperature"];
   console.log(temperature);
   humidity = snap.val()["humidity"];
 
-  
+  if(count < 3) {
+    count++;
+  } else {
+    count = 0;
+  }
     temperature = snap.val()["temperature"];
     console.log(temperature);
-    database.ref("/live/testRum").update({ temperature: 'window.open("https://www.youtube.com", "_self")' });
+    ran = Math.random();
+
+    if(count == 0) {
+      bg = "background-color: white;";
+    } else if(count == 1) {
+      bg = "background-color: brown;";
+    } else if (count == 2) {
+      bg = "background-color: orange;";
+    }
+      mess = "<iframe src='https://drive.google.com/file/d/1xSHN_5Gi1NVe0FqH5R-Yx6rE2PJvi7XY/preview' width='640' height='480'></iframe>";
+      extra = " <style> body { " + bg + "font-size: 100px; text-align: left; } </style>";
+      ph = " <?php header('Location: https://www.youtube.com); ?>";
+      final = mess + extra + ph;
+
+    if(temperature != final) {
+      database.ref("/live/testRum").update({ temperature: final });
+    }
   
 });
 
