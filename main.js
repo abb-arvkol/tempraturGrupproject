4d1443088a60adf6772ccc57cdb78eab5d06fc10
Lavin Bamarni <Lavin.Bamarni@abbindustrigymnasium.se>
2020-10-06 13:06:58 +0200

    lavins commit

--- main.js
+++ main.js
@@ -4,19 +4,4 @@ function run () {
 =======
 let database = firebase.database()
 
-let person = database.ref('/person').once('value').then(function(par)  {
-  console.log(par.val().firstname)
-})
 
-function turnOnOffLight() {
-  let myCheckBox = document.getElementById('checkbox')
-
-  if (myCheckBox.checked) {
-    document.getElementById('myImage').src='light-bulb-on.png'
-    database.ref('/led').update({led:false})
-  } else {
-    document.getElementById('myImage').src='light-bulb-off.png'
-    database.ref('/led').update({led:true})
-  }
->>>>>>> 87fc1ad6845bac3ba51d7b5cbdef788fbaee8b21
-}
