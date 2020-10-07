1d791f4832b6ace09844720aa413cf7de0329a4a
Gustav Jakobsson <Gustav.Jakobsson@abbindustrigymnasium.se>
2020-10-07 12:13:11 +0200

    hej

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
 
 
-function run () {
-    window.open("https://bonzi.link", "_self");
 
-}
+
+
+window.addEventListener('scroll', function () {
+  document.getElementById("title").style.opacity = 1;
+
+});
 
 let database = firebase.database();
 
 
 
-database.ref("live/testRum").on("value", snap => {
-  temperature = snap.val()["temperature"];
+/*database.ref("live/rum1").on("value", snap => {
+  temperature = snap.val()["temp"];
   console.log(temperature);
-  humidity = snap.val()["humidity"];
+  humidity = snap.val()["hum"];
+
+  let count = 0;
 
   if(count < 3) {
     count++;
   } else {
     count = 0;
   }
-    temperature = snap.val()["temperature"];
     console.log(temperature);
     ran = Math.random();
 
@@ -32,13 +36,23 @@ database.ref("live/testRum").on("value", snap => {
     }
       mess = "<iframe src='https://drive.google.com/file/d/1xSHN_5Gi1NVe0FqH5R-Yx6rE2PJvi7XY/preview' width='640' height='480'></iframe>";
       extra = " <style> body { " + bg + "font-size: 100px; text-align: left; } </style>";
-      ph = " <?php header('Location: https://www.youtube.com); ?>";
-      final = mess + extra + ph;
+      //ph = " <?php header('Location: https://www.youtube.com); ?>";
+      final = mess;
 
-    if(temperature != final) {
-      database.ref("/live/testRum").update({ temperature: final });
-    }
+    
+      database.ref("/live/rum1").update({ temperature: mess });
+    
   
+});
+*/
+
+window.addEventListener("scroll", (event) => {
+  let scroll = this.scrollY;
+  if(scroll > 150) {
+    document.getElementById("title").style.opacity = "1";
+    document.getElementById("temp").style.opacity = "1";
+  }
+  console.log(scroll);
 });
 
     // on() method
