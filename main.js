9b4a189508b20cd21fb83e04d5523074a678768a
Arvid Kolsmyr <Arvid.Kolsmyr@abbindustrigymnasium.se>
2020-10-13 08:38:31 +0200

    städade koden + fixade decimalantal på temperaturen

--- main.js
+++ main.js
@@ -7,44 +7,9 @@ database.ref("live/rum1").on("value", snap => {//checks if value paramater chang
   console.log(temperature);
   humidity = snapval["hum"];// sets humitity to the value in humitity in the snapval object
 
-  document.getElementById("temp").innerHTML = `temperature:${temperature}℃`;
-  document.getElementById("hum").innerHTML =`humidity:${humidity}`;
+  document.getElementById("temp").innerHTML = `temperature:${temperature.toFixed(5)}℃`;//toFixed method specifies number of decimals shown
+  document.getElementById("hum").innerHTML =`humidity:${humidity.toFixed(5)}`;
 });
-
-
-/*database.ref("live/rum1").on("value", snap => {
-  temperature = snap.val()["temp"];
-  console.log(temperature);
-  humidity = snap.val()["hum"];
-
-  let count = 0;
-
-  if(count < 3) {
-    count++;
-  } else {
-    count = 0;
-  }
-    console.log(temperature);
-    ran = Math.random();
-
-    if(count == 0) {
-      bg = "background-color: white;";
-    } else if(count == 1) {
-      bg = "background-color: brown;";
-    } else if (count == 2) {
-      bg = "background-color: orange;";
-    }
-      mess = "<iframe src='https://drive.google.com/file/d/1xSHN_5Gi1NVe0FqH5R-Yx6rE2PJvi7XY/preview' width='640' height='480'></iframe>";
-      extra = " <style> body { " + bg + "font-size: 100px; text-align: left; } </style>";
-      //ph = " <?php header('Location: https://www.youtube.com); ?>";
-      final = mess;
-
-    
-      database.ref("/live/rum1").update({ temperature: mess });
-    
-  
-});
-*/
 
 window.addEventListener("scroll", (event) => {
   let scroll = this.scrollY;
